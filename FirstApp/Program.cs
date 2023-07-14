10fe59f5d39f6300702c86a68354b44a50b384e8
Samuel Legrain <samuel.legrain@bstorm.be>
2023-07-14 17:02:10 +0200

    Ajoutez des fichiers projet.

---
+++ FirstApp/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace FirstApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.Write("Veuillez entrer votre nom : ");
+            string nomUtilisateur = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("Hello, {0}!", nomUtilisateur);
+            Console.ReadLine();
+        }
+    }
+}

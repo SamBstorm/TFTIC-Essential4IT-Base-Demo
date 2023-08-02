1ea91127e9a178f2327147094c2c4ffc5fb473cc
Samuel Legrain <samuel.legrain@bstorm.be>
2023-08-02 15:51:11 +0200

    Add Demo Boucle + Collection

---
+++ Demo-Boucle/Program.cs
@@ -0,0 +1,79 @@
+﻿#region Exemple boucle while
+/*
+
+Console.WriteLine("Veuillez introduire un nombre");
+string user_response = Console.ReadLine();
+
+int response;
+int counter = 1;
+
+
+while (!int.TryParse(user_response, out response) && counter < 3)
+{
+    Console.WriteLine($"Erreur, \"{user_response}\" n'est pas un nombre...\n\tVeuillez introduire un nombre :");
+    user_response = Console.ReadLine();
+    counter++;
+}
+
+if (int.TryParse(user_response, out _))
+    Console.WriteLine($"Le chiffre introduit est {response}.\nVous avez fait {counter} tentative{((counter > 1) ? "s" : "")}.");
+else
+    Console.WriteLine($"Vous avez dépassé les 3 tentatives..."); 
+*/
+#endregion
+
+#region Exemple boucle do...while
+/*
+string message = "Veuillez introduire un nombre";
+string user_response;
+
+int response;
+int counter = 0;
+
+
+do
+{
+    Console.WriteLine(message);
+    user_response = Console.ReadLine();
+    message = $"Erreur, \"{user_response}\" n'est pas un nombre...\n\tVeuillez introduire un nombre :";
+    counter++;
+} while (!int.TryParse(user_response, out response) && counter < 3);
+
+if (int.TryParse(user_response, out _))
+    Console.WriteLine($"Le chiffre introduit est {response}.\nVous avez fait {counter} tentative{((counter > 1) ? "s" : "")}.");
+else
+    Console.WriteLine($"Vous avez dépassé les 3 tentatives...");
+*/
+#endregion
+
+#region Exemple boucle foreach
+
+string[] names = {
+    "Gaëlle",
+    "Cassandra",
+    "Sébastien",
+    "An-lac",
+    "Loïc",
+    "Kévin",
+    "Maxime",
+    "Valérian",
+    "Jordan"
+};
+
+foreach (string name in names)
+{
+    Console.WriteLine(name);
+}
+
+// Equivalent de la boucle foreach au dessus
+for (int i = 0; i < names.Length; i++)
+{
+    string name = names[i];
+    Console.WriteLine(name);
+}
+//BONUS
+int indice = Array.IndexOf(names, "Loïc");
+if(indice ==-1) Console.WriteLine("Loïc n'est pas là!");
+else Console.WriteLine($"Loïc est en indice {indice}.");
+
+#endregion
---
+++ Demo-Collection/Program.cs
@@ -0,0 +1,95 @@
+﻿#region Démonstration List<T>
+/*
+List<string> shoplist = new List<string>();
+#region Exemple sans boucle
+//shoplist.Add("Papier toilette");
+//shoplist.Add("Savon");
+
+//string[] necessaireBucal = new string[] { "Brosse à dent", "Dentifrice", "Bain bucale", "Fil detaire" };
+//shoplist.AddRange(necessaireBucal); 
+#endregion
+
+#region Exemple avec boucle
+Console.WriteLine("Veuillez indiquer les produits nécessaires :");
+string input = Console.ReadLine();
+
+while (input.ToUpper() != "FIN")
+{
+shoplist.Add(input);
+Console.WriteLine("Veuillez indiquer les produits nécessaires :");
+input = Console.ReadLine();
+}
+
+#endregion
+
+shoplist.Sort();
+
+foreach (string product in shoplist)
+{
+Console.WriteLine(product);
+}
+
+Console.WriteLine("Donner le nom d'un élément de la liste à supprimer :");
+string toRemove = Console.ReadLine();
+
+if (shoplist.Contains(toRemove))
+{
+shoplist.Remove(toRemove);
+Console.WriteLine($"L'élément \"{toRemove}\" est supprimé.");
+}
+else
+{
+Console.WriteLine($"L'élément \"{toRemove}\" ne figure pas dans la liste.");
+}
+
+foreach (string product in shoplist)
+{
+Console.WriteLine(product);
+}
+*/
+#endregion
+
+#region Démonstration Dictionnary<TKey,TValue>
+
+Dictionary<string,int> shoplist = new Dictionary<string,int>();
+
+Console.WriteLine("Veuillez indiquer le produit à acheter:");
+string product_name = Console.ReadLine().ToUpper().Trim();
+
+while (product_name.ToLower() != "fin")
+{
+    if (shoplist.ContainsKey(product_name))
+    {
+        Console.WriteLine($"Actuellement la quantité de {product_name} est de {shoplist[product_name]}, combien en voulez-vous ?");
+        string quantity_input = Console.ReadLine();
+        int quantity;
+        while (!int.TryParse(quantity_input, out quantity))
+        {
+            Console.WriteLine("Veuillez introduire une quantité...");
+            quantity_input = Console.ReadLine();
+        }
+        shoplist[product_name] = quantity;
+    }
+    else
+    {
+        Console.WriteLine($"Combien de {product_name} vous faut-il?");
+        string quantity_input = Console.ReadLine();
+        int quantity;
+        while (!int.TryParse(quantity_input, out quantity))
+        {
+            Console.WriteLine("Veuillez introduire une quantité...");
+            quantity_input = Console.ReadLine();
+        }
+        shoplist.Add(product_name, quantity);
+    }
+    if (shoplist[product_name] == 0) shoplist.Remove(product_name);
+    Console.WriteLine("Veuillez indiquer le produit à acheter:");
+    product_name = Console.ReadLine().ToUpper().Trim();
+}
+
+foreach (KeyValuePair<string,int> kvp in shoplist)
+{
+    Console.WriteLine($"{kvp.Key} : {kvp.Value}");
+}
+
+#endregion

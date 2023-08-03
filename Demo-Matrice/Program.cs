c366fa6a4d1e0b9a9014dab74a09609535c935d8
Samuel Legrain <samuel.legrain@bstorm.be>
2023-08-03 10:18:17 +0200

    Add Demo Matrice

---
+++ Demo-Matrice/Program.cs
@@ -0,0 +1,23 @@
+﻿const int TAILLE_X = 3;
+const int TAILLE_Y = 3;
+
+string[,] grille = new string[TAILLE_X, TAILLE_Y];
+
+for (int i = 0; i < TAILLE_X; i++)
+{
+	for (int j = 0; j < TAILLE_Y; j++)
+	{
+		grille[i, j] = "X";
+	}
+}
+
+grille[1, 1] = "O";
+
+for (int i = 0; i < TAILLE_X; i++)
+{
+    for (int j = 0; j < TAILLE_Y; j++)
+    {
+        Console.Write(grille[i, j]);
+    }
+    Console.WriteLine();
+}
